acc2d0e3ff4a6df4cc4ccfbcac9dd5a037149ae3
Hongseob Lee <sjun2918@naver.com>
2023-10-01 14:55:25 +0900

    project init

---
+++ src/main/java/com/example/cicdpractice/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.cicdpractice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@Value("${app.version:1.0.0}")
+	String version;
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello CI/CD! Version: " + version;
+	}
+}
